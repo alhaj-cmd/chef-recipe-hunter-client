59c29fe1199a7baedcdc170621ca892ec1e402ba
github_username <you@domain.com>
2023-05-01 22:42:10 +0600

    firebase & react bootstrap install

---
+++ src/firebase/Firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnjAyI9xSA7m7emX0d24-GudzlI2Gv4wk",
+  authDomain: "chef-recipe-hunter-dae40.firebaseapp.com",
+  projectId: "chef-recipe-hunter-dae40",
+  storageBucket: "chef-recipe-hunter-dae40.appspot.com",
+  messagingSenderId: "588486035131",
+  appId: "1:588486035131:web:74f225acc815cea902cb9e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+ export default app;
